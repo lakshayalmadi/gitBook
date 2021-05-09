4074ddb8f4450f9049c0f85b74bddb9f35493a87
lakshayalmadi <lashay.almadi.la@gmail.com>
2021-05-10 01:57:26 +0530

    removed-null

--- js/index.js
+++ js/index.js
@@ -1,28 +1,36 @@
-const GITHUBAPI= 'https://api.github.com/users/';
-//localStorage.setItem('uiTheme', 'dark')
+const GITHUBAPI = "https://api.github.com/users/";
+localStorage.setItem("uiTheme", "dark");
 
-const main = document.getElementById('main');
-const form = document.getElementById('form');
-const search =  document.getElementById('search');
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
 
 const toggle = document.getElementById("toggle");
 
 toggle.addEventListener("change", (e) => {
     document.body.classList.toggle("light-mode", e.target.checked);
-    document.getElementsByClassName('card').classList.toggle("light-mode-items", e.target.checked);
-    document.getElementById('search').classList.toggle("light-mode-items", e.target.checked);
-    //localStorage.setItem('uiTheme', localStorage.getItem('uiTheme')==='dark'?'light':'dark')
-    //if(localStorage.getItem('uiTheme')==='dark'){
-    //    document.getElementsByClassName('card').setAttribute('style', 'background-color: white');
-    //}
-    //else{
-    //    document.getElementsByClassName('card').removeAttribute('style');
-    //}
+    localStorage.setItem(
+        "uiTheme",
+        localStorage.getItem("uiTheme") === "dark" ? "light" : "dark"
+    );
+    uiThemeHandler();
 });
 
-async function getUser(username){
+function uiThemeHandler() {
+    const cardElement = document.querySelector("#card");
+    if(!cardElement) {
+        return;
+    }
+    if (localStorage.getItem("uiTheme") === "light") {
+        cardElement.setAttribute("style", "background-color: white");
+    } else {
+        cardElement.removeAttribute("style");
+    }
+}
+
+async function getUser(username) {
     const response = await fetch(GITHUBAPI + username);
-    const responseData= await response.json();
+    const responseData = await response.json();
 
     createUserCard(responseData);
     getRepos(username);
@@ -51,20 +59,20 @@ function addReposToCard(repos) {
         });
 }
 
-function createUserCard(user){
-
-    const cardHTML =`
+function createUserCard(user) {
+    const cardHTML = `
         <div class="card" id="card">
             <div class="image-container">
-                <img class="profile-img" src="${user.avatar_url}" alt="${user.name}"/>
+                <img class="profile-img" src="${user.avatar_url}" alt="${
+        user.name
+    }"/>
             </div>
             <div class="user-info">
                 <h2><a href="${user.html_url}">${user.name}</a></h2>
-                <p>${user.bio}</p>
-                <p>${user.company}</p>
-                <p>${user.location}</p>
-                <p>${user.blog}</p>    
-
+                ${user.bio ? `<p>${user.bio}</p>` : ``}
+                ${user.company ? `<p>${user.company}</p>` : ``}
+                ${user.location ? `<p>${user.location}</p>` : ``}
+                ${user.blog ? `<p>${user.blog}</p>` : ``}
                 <ul class="info">
                     <li><strong>Followers: ${user.followers}</strong></li>
                     <li><strong>Following: ${user.following}</strong></li>
@@ -76,20 +84,18 @@ function createUserCard(user){
 
         `;
 
-        main.innerHTML = cardHTML;
+    main.innerHTML = cardHTML;
+    uiThemeHandler();
 }
 
-form.addEventListener('submit', e =>{
+form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const user = search.value;
 
-    if(user){
+    if (user) {
         getUser(user);
 
-        search.value='';
+        search.value = "";
     }
-    main.innerHTML = cardHTML;
-    }
-
 });
